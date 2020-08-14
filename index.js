b0df28ed20c3191f67179db5568e621cf77619ae
Marc Altmann <marcaltmann@posteo.de>
2020-08-14 22:23:35 +0200

    Make port configurable

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
@@ -9,4 +10,6 @@ const server = http.createServer((req, res) => {
     });
 });
 
-server.listen(3000);
+server.listen(port);
+
+console.log(`Server listening on port ${port}.`);
