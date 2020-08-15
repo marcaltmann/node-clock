89ae2543a1f59310c5ea1789939370945137de55
Marc Altmann <marcaltmann@posteo.de>
2020-08-15 08:43:07 +0200

    Add moment-timezone

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const moment = require('moment');
+require('moment-timezone');
+
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
   let date = moment();
-  res.render('index', { date: date.format('LLL') });
+  let formattedDate = date.tz('Europe/Berlin').format('LLL');
+  res.render('index', { date: formattedDate });
 });
 
 app.get('/error', (req, res) => {
